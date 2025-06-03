ebbdbf6c5d6272c0ae14ba46a9e586c883a16d9a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-06-03 18:25:40 -0400

    Updated paragraph text

---
+++ src/styles/design-system.ts
@@ -0,0 +1,6 @@
+export const body = {
+  sm: "text-sm leading-loose text-balance tracking-wide",
+  md: "text-base leading-loose text-balance tracking-wide",
+  lg: "text-lg leading-loose text-balance tracking-wide",
+  xl: "text-xl leading-loose text-balance tracking-wide",
+};
