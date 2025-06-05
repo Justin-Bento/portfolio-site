cc4936bae1661edbfa6505b1d685d2787158d17c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-06-05 16:44:27 -0400

    Installed the Vercel CLI for astrojs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
