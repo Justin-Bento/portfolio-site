a05ac1bfc183067fcd1ce397011d114db463314e
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-06-18 23:16:44 -0400

    Fixed spelling mistake.

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -102,7 +102,7 @@ const siteConfig: SiteConfig = {
             src: '/justin-bento-preview.webp',
             alt: 'A person sitting standing in a recording studio behind a drumkit set.'
         },
-        // Light theme spxsecific image
+        // Light theme specific image
         lightThemeImage: {
             src: '/justin-bento-light.jpg',
             alt: 'A person standing in a recording studio behind a drumkit set - light version'
