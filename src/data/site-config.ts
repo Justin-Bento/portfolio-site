efd8e851e58c6c2b4175c7e8d2d847e907097d37
Justin Bento <44421842+Justin-Bento@users.noreply.github.com.>
2025-06-20 18:34:59 -0400

    Replaced space with dash.

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -43,7 +43,7 @@ export type SiteConfig = {
 const siteConfig: SiteConfig = {
     website: 'https://www.justin-bento.com/',
     title: 'Justin Bento',
-    subtitle: 'Full Stack Website Developer',
+    subtitle: 'Full-Stack Website Developer',
     description: 'This is a portfolio website for Justin Bento. It serves as a central hub to showcase his skills, experience, and work samples.',
     image: {
         src: '/justin-bento-dark.webp',
